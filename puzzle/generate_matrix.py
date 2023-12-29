77eb45cb1d3705f3ebf35f722b488be0c7ba4f8a
Benjamin Lichtenstein <31620900+bennilic@users.noreply.github.com>
2023-12-29 20:16:46 +0100

    Exchanges generate random state method with the one that uses random generator instead of permutation.

--- puzzle/generate_matrix.py
+++ puzzle/generate_matrix.py
@@ -3,10 +3,16 @@ import numpy as np
 
 class GenerateMatrix:
     @staticmethod
-    def generate_random_state():
-        return np.random.permutation(9).reshape(3, 3)
-
-    @staticmethod
     def generate_goal_state():
         goal_matrix = np.arange(9).reshape((3, 3))
         return goal_matrix
+
+    @staticmethod
+    def generate_random_state(seed: int = None) -> np.ndarray:
+        rng = np.random.default_rng(seed)
+
+        numbers = np.arange(9)
+        rng.shuffle(numbers)
+
+        puzzle = np.reshape(numbers, (3, 3))
+        return puzzle
