f63f4fb737524a4049f44900d3411e6407fb60c1
Alexander Kroneiser <113134616+Vastitus@users.noreply.github.com>
2024-01-07 15:31:12 +0100

    Added Doctype comments.

--- puzzle/generate_matrix.py
+++ puzzle/generate_matrix.py
@@ -5,7 +5,9 @@ class GenerateMatrix:
     @staticmethod
     def generate_goal_state() -> np.ndarray:
         """
-        Generates the goal state matrix for the 8-puzzle problem
+        Generates the goal state matrix for the 8-puzzle problem.
+
+        :return: np.ndarray: A 3x3 NumPy array representing the goal state matrix for the 8-puzzle problem.
         """
         goal_matrix = np.arange(9).reshape((3, 3))
         return goal_matrix
@@ -14,6 +16,10 @@ class GenerateMatrix:
     def generate_random_state(seed: int = None) -> np.ndarray:
         """
         Generates a random initial state matrix for the 8-puzzle problem.
+
+        :param seed (int): Seed value for reproducibility of random state generation. Defaults to None.
+
+        :return: np.ndarray: A 3x3 NumPy array representing a random initial state matrix for the 8-puzzle problem.
         """
         rng = np.random.default_rng(seed)
 
